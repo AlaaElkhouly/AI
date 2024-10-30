8ffc5af275d448d9347b5382d55d887118974992
perfectdude719 <147012319+perfectdude719@users.noreply.github.com>
2024-10-30 10:15:09 +0300

    asd

--- 8_puzzle/Astar_test.py
+++ 8_puzzle/Astar_test.py
@@ -1,5 +1,8 @@
 import Astar
 start_state=[0,4,7,5,2,3,8,6,1]
 flag=1 # euclidean
-
-print(Astar.Astar(start_state,flag))
+cost, current_state, path, previous_moves =Astar.Astar(start_state,flag)
+print(cost)
+print(current_state)
+print(path)
+print(previous_moves)
